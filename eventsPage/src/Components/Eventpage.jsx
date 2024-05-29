7831147de0577b5b802918f3af1df15da5e34a56
Khushi Aggarwal <khushiaggarwal300@gmail.com>
2024-05-30 00:41:37 +0530

    added basic react

---
+++ eventsPage/src/Components/Eventpage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Eventpage.css";
+
+const Eventpage = () => {
+  return (
+    <>
+      <h1>AI CONCLAVE 4.0</h1>
+      <div className="event-container">
+        <div className="left-event-container">
+          <p className="context">
+            Immerse yourself in industry insights through ideations, hackathons
+            , and expert keynotes. Explore NLP, MLOps , Computer Vision, etc.
+            Experience hands-on workshops.
+          </p>
+          <button className="register-button">Register Now</button>
+        </div>
+        <div className="right-event-container">
+          <img
+            src="https://source.unsplash.com/random/500x250?technology"
+            alt="Technology"
+          />
+          <div className="text">
+            Welcome to AI Conclave 2024, A 3-day thrill ride from February 5th
+            to 7th, hosted by the AI Club at VIT Bhopal University.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Eventpage;
