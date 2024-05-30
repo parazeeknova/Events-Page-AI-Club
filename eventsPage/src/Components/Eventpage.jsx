553cca75ad97d7ac74856f5e660ab1947793a92f
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-05-30 16:56:41 +0530

    Add GSAP for highlighter effect
    Barely working atm.. Need optimizations

--- eventsPage/src/Components/Eventpage.jsx
+++ eventsPage/src/Components/Eventpage.jsx
@@ -1,18 +1,40 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { useEffect} from "react";
+import { gsap, ScrollTrigger } from "gsap/all";
 import "./Eventpage.css";
 
 const Eventpage = () => {
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    gsap.utils.toArray('.highlight').forEach(highlight => {
+      gsap.fromTo(highlight.querySelector('.highlight-bg'), 
+        { width: "0%" }, 
+        { 
+          width: "130%", 
+          scrollTrigger: {
+            trigger: highlight,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+          }
+        }
+      );
+    });
+  }, []);
+
   return (
     <>
       <div className="event-container">
 
         <div className="left-event-container">
-          <p className="context">
-            Immerse yourself in industry insights through ideations, hackathons,
-            and expert keynotes. Explore NLP, MLOps , Computer Vision, etc.
-            Experience hands-on workshops.
-          </p>
+        <p className="context">
+  Immerse yourself in <span className="highlight"><span className="highlight-bg"></span>industry</span> insights through ideations, <span className="highlight"><span className="highlight-bg"></span>hackathons</span>,
+  and expert keynotes. Explore <span className="highlight"><span className="highlight-bg"></span>NLP, MLOps</span> , Computer Vision, etc.
+  <span className="highlight"><span className="highlight-bg"></span>Experience</span> hands-on workshops.
+</p>
           <button className="register-button">Register Now</button>
         </div>
 
