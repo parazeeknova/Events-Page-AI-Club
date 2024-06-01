063ca46770c33e50662b53bbde2f24e50c573139
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-06-01 14:12:07 +0530

    Add PDF viewer functionality
    Currently just works with local pdfs

--- magazinePage/src/App.jsx
+++ magazinePage/src/App.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
+import Sample from './components/magazinePage';
 
 const App = () => {
   return (
     <div>
-      <h1>Magazine Page</h1>
+      <Sample />
     </div>
   )
 }
---
+++ magazinePage/src/components/magazinePage.jsx
@@ -0,0 +1,86 @@
+import { useCallback, useState } from "react";
+import { useResizeObserver } from "@wojtekmaj/react-hooks";
+import { pdfjs, Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+import "./magazinePage.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+const options = {
+  cMapUrl: "/cmaps/",
+  standardFontDataUrl: "/standard_fonts/",
+};
+
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+export default function Sample() {
+  const [file, setFile] = useState("./sample.pdf");
+  const [numPages, setNumPages] = useState();
+  const [containerRef, setContainerRef] = useState(null);
+  const [containerWidth, setContainerWidth] = useState();
+
+  const onResize = useCallback((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onFileChange(event) {
+    const { files } = event.target;
+
+    const nextFile = files?.[0];
+
+    if (nextFile) {
+      setFile(nextFile);
+    }
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div className="Example">
+      <header>
+        <h1>AI Club's Magazine</h1>
+      </header>
+      <div className="Example__container">
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>{" "}
+          <input onChange={onFileChange} type="file" />
+        </div>
+        <div className="Example__container__document" ref={setContainerRef}>
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <div className="page-container" key={`page_${index + 1}`}>
+                <Page
+                  pageNumber={index + 1}
+                  width={
+                    containerWidth
+                      ? Math.min(containerWidth, maxWidth)
+                      : maxWidth
+                  }
+                />
+              </div>
+            ))}
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+}
