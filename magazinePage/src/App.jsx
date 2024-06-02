f01e358134dd5ff79ab5b8c75974ef3c24b7126a
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-06-02 17:21:47 +0530

    Added Navpill to match the Design
    TODO: Adding a bottom bar for the change in edition

--- magazinePage/src/App.jsx
+++ magazinePage/src/App.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import Sample from './components/magazinePage';
+// import Sample from './components/magazinePage';
 import Flipbook from './components/Flipbook';
 
 const App = () => {
--- magazinePage/src/components/Flipbook.jsx
+++ magazinePage/src/components/Flipbook.jsx
@@ -10,6 +10,7 @@ const Pages = React.forwardRef((props, ref) => {
   return (
     <div className="demoPage" ref={ref}>
       {/* <h1>Page Header</h1> */}
+      {/*eslint-disable-next-line react/prop-types*/}
       <p>{props.children}</p>
     </div>
   );
@@ -42,11 +43,37 @@ function Flipbook() {
 
   return (
     <>
-      <div className="nav-bar">
-        <button onClick={goToPrevPage}>Previous page</button>
-        <span>{pageNumber} / {numPages}</span>
-        <button onClick={goToNextPage}>Next page</button>
+
+      <div className="flipbook-container">
+        <div className="nav-bar">
+          <div className="previousButton">
+            <button onClick={goToPrevPage}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+                <path d="M4 12L20 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M8.99996 17C8.99996 17 4.00001 13.3176 4 12C3.99999 10.6824 9 7 9 7" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </button>
+          </div>
+          <div className="middleButton">
+            <span>{pageNumber}</span>
+              <div className="middleSymbol"> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+                  <path d="M21 4C11.0535 4 12.9465 20 3 20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </div>  
+            <span>{numPages}</span>
+          </div>
+          <div className="nextButton">
+            <button onClick={goToNextPage}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+                <path d="M20 12L4 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M15 17C15 17 20 13.3176 20 12C20 10.6824 15 7 15 7" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
+
       <div className="mag-container">
         <HTMLFlipBook ref={book} width={350} height={500} showCover={true}>
           {[...Array(numPages).keys()].map((n) => (
@@ -64,6 +91,7 @@ function Flipbook() {
           ))}
         </HTMLFlipBook>
       </div>
+
     </>
   );
 }
